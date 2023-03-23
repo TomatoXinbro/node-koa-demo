035b96d9019ea209531ab395a86b01c674f094b6
xin <1121592239@qq.com>
2023-03-23 16:33:00 +0800

    🎉 init(划分目录结构):

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const app = require("./app/index");
 const config = require("./app/config");
 
-app.listen(8888, () => {
+app.listen(config.APP_POST, () => {
   console.log(`服务器在端口${config.APP_POST}启动成功哦!!`);
 });
