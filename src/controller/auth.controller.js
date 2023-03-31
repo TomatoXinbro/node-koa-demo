d34a97e22280382499c169b440ab7314b6be151c
xin <1121592239@qq.com>
2023-03-31 15:43:45 +0800

    ✨ feat(鉴权逻辑的完善 ===>传递User参数):

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -6,21 +6,26 @@ const { PRIVATE_KEY } = require("../app/config");
 // console.log(PRIVATE_KEY.toString());
 class LoginController {
   create(ctx, next) {
-    const { username } = ctx.request.body;
-    console.log(PRIVATE_KEY);
+    const { id, name } = ctx.user; // 把用户信息加载进payload里面 以便解析token的时候知道这个token是谁登录的
     try {
-      const token = jwt.sign({ user: username }, PRIVATE_KEY, {
+      const token = jwt.sign({ id, name }, PRIVATE_KEY, {
         algorithm: "RS256", // 加密算法 默认HS265 使用非对称加密时要指定加密算法
         expiresIn: 60 * 60 * 24, // 过期时间 24小时
       });
-      // console.log(token);
-      ctx.body = dataFormate(200, LOGIN_SUCCESS, { token: token });
+
+      ctx.body = dataFormate(200, LOGIN_SUCCESS, {
+        id,
+        name,
+        token,
+      });
     } catch (error) {
       console.log(error);
     }
   }
+
   authSuccess(ctx, next) {
     ctx.body = dataFormate(200, "token鉴权通过");
+    console.log(ctx.user.id);
   }
 }
 
